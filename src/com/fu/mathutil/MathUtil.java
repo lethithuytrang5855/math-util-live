dc8168ba66cc31f7133b4eac7c0f6b17e88eb1ff
lethithuytrang5855 <lethithuytrang5855@gmail.com>
2023-02-27 14:03:13 +0700

    Dua code len server lan1

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MathUtil {
+        public static long getFactorial(int n){
+            if(n<0 || n>20){
+                throw new IllegalArgumentException("ERROR!");
+            }
+            if(n==0 || n==1){
+                return 1;
+            }
+            long s=1;
+            for(int i=2; i<= n; i++)         
+            s *=i;            
+            return s;
+          
+        }
+}
